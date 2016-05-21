0c73cabc23273f2934f6083483156eb10e53bfa6
S. Mannem <sebas@mannem.nl>
2016-05-21 10:24:12 +0200

    some skeleton and working solution with reading POST query items (globalsession) in REST_server.py

--- REST_server.py
+++ REST_server.py
@@ -6,8 +6,23 @@ import psycopg2
 
 urls = (
     '/tables', 'list_tables',
-    '/tables/(.*)', 'get_table',
+
+    '/session/start', 'session_start',
+    '/transaction/begin', 'transaction_begin',
+
+    '/query/execute', 'query_execute',
+    '/query/getone', 'query_get_one_record',
+    '/query/getall', 'query_get_all_records',
+    '/query/rowcount', 'query_numrecords',
+
+    '/transaction/commit', 'transaction_commit',
+    '/transaction/rollback', 'transaction_rollback',
+    '/session/close', 'session_close',
 )
+
+def return_json(myDict):
+    web.header('Content-Type', 'application/json')
+    return json.dumps(myDict)
 
 class list_tables:
     def GET(self):
@@ -20,10 +35,38 @@ class list_tables:
             t['schema'] = r[0]
             t['table'] = r[1]
             tables.append(t)
-        web.header('Content-Type', 'application/json')
-        return json.dumps(tables)
+        return return_json(tables)
 
-class get_table:
+class session_start:
+    def GET(self):
+        i=web.input()
+        print(i.globalsession)
+        ret={}
+        ret['sid'] = 1
+        return return_json(ret)
+
+class transaction_begin:
+    def GET(self):
+        ret={}
+        ret['sid'] = 3
+        ret['tid'] = 2
+        return return_json(ret)
+
+class query_execute:
+    def GET(self):
+        ret={}
+        ret['sid'] = 3
+        ret['tid'] = 2
+        return return_json(ret)
+
+class query_get_one_record:
+    def GET(self):
+        ret={}
+        ret['sid'] = 3
+        ret['tid'] = 2
+        return return_json(ret)
+
+class query_get_all_records:
     def GET(self, table):
         c=psycopg2.connect("host='localhost' dbname='sabes'")
         cur=c.cursor()
@@ -32,7 +75,28 @@ class get_table:
         for r in cur:
             table.append(r)
         web.header('Content-Type', 'application/json')
-        return json.dumps(table)
+        return return_json(table)
+
+class query_numrecords:
+    def GET(self):
+        ret={}
+        ret['sid'] = 3
+        ret['tid'] = 2
+        return return_json(ret)
+
+class transaction_commit:
+    def GET(self):
+        ret={}
+        ret['sid'] = 3
+        ret['tid'] = 2
+        return return_json(ret)
+
+class session_close:
+    def GET(self):
+        ret={}
+        ret['sid'] = 3
+        ret['tid'] = 2
+        return return_json(ret)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
