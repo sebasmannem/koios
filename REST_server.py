5e28466ecc50d83004c784024b05f8f570af0177
S. Mannem <sebas@mannem.nl>
2016-05-23 08:43:20 +0200

    split REST class and DB class

--- REST_server.py
+++ REST_server.py
@@ -9,26 +9,51 @@ urls = (
     '/db/(.*)', 'REST_db',
 )
 
-mySessions = {}
-
 class REST_db:
+
+    sessions = {}
+
     def GET(self, sub):
         sub=sub.split('/')
-        try:
-            methodToCall = getattr(self, sub[0])
-            ret=methodToCall(sub[1:])
-        except Exception as e:
-            print(e)
-            raise web.notfound()
-            return ""
+        i=web.input()
+        if sub[0] == 'session':
+            if sub[1] == 'open':
+                s = db_session(i)
+                self.sessions[s.ID] = s
+                ret = s.ID
+            elif sub[1] == 'close':
+                del self.sessions[i.sid]
+                ret = i.sid
+        else:
+            try:
+                s = self.sessions[i.gid]
+                methodToCall = getattr(s, sub[0])
+                ret=methodToCall(sub[1:], i)
+            except Exception as e:
+                print(e)
+                raise web.notfound()
+                return ""
         web.header('Content-Type', 'application/json')
         return json.dumps(ret)
+
     def POST(self, sub):
         web.header('Content-Type', 'application/json')
         return json.dumps(tables)
-    def tables(self, sub):
-        c=psycopg2.connect("host='localhost' dbname='sabes'")
-        cur=c.cursor()
+
+class db_session():
+
+    def __init__(self, i):
+        self.cn = psycopg2.connect("host='localhost' dbname='{0}'".format(i.db))
+        self.transactions = []
+        self.ID = str(uuid.uuid4())
+        self.gid = i.gid
+
+    def __del__(self):
+        self.cn.rollback()
+        self.cn.close()
+
+    def tables(self, sub, i):
+        cur=self.cn.cursor()
         tables=[]
         cur.execute('select schemaname, tablename from pg_tables')
         for r in cur:
@@ -37,21 +62,8 @@ class REST_db:
             t['table'] = r[1]
             tables.append(t)
         return tables
-    def session(self, sub):
-        i=web.input()
-        if sub[0] == 'open':
-            mySessionID = str(uuid.uuid4())
-            self.globalID = i.gid
-            mySessions[mySessionID] = self
-        elif sub[0] == 'close':
-            mySessionID = i.UUID
-            del mySessions[mySessionID]
-        else:
-            raise KeyError("Invalid REST call")
-        return mySessionID
 
-    def transaction(self, sub):
-        i=web.input()
+    def transaction(self, sub, i):
         if sub[0] == 'begin':
             mySessionID = i.sid
             mytransID = str(uuid.uuid4())
@@ -61,28 +73,22 @@ class REST_db:
             mytransID = i.tid
         return mytransID
 
-    def query_execute(self, sub):
-        ret={}
-        ret['sid'] = 3
-        ret['tid'] = 2
+    def query_execute(self, sub, i):
+        if sub[0] == 'execute':
+            ret={}
+            ret['sid'] = 3
+            ret['tid'] = 2
+        elif sub[0] == 'get':
+            cur=self.cn.cursor()
+            cur.execute('select * from '+table)
+            ret=[]
+            for r in cur:
+                ret.append(r)
+        elif sub[0] == 'num':
+            ret={}
+            ret['sid'] = 3
+            ret['tid'] = 2
         return ret
-
-    def query_get(self, sub):
-        c=psycopg2.connect("host='localhost' dbname='sabes'")
-        cur=c.cursor()
-        cur.execute('select * from '+table)
-        table=[]
-        for r in cur:
-            table.append(r)
-        web.header('Content-Type', 'application/json')
-        return table
-
-    def query_num(self, sub):
-        ret={}
-        ret['sid'] = 3
-        ret['tid'] = 2
-        return ret
-
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
